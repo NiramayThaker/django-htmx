58a206215850d1cfa4911900fdaeff2137c6f35e
NiramayThaker <thakerniramay@gmail.com>
2023-09-27 23:29:36 +0530

    Creating models from json data

--- gdp/management/commands/populate.py
+++ gdp/management/commands/populate.py
@@ -1,8 +1,29 @@
 from django.core.management.base import BaseCommand
+from gdp.models import GDP
+from django.conf import settings
+import json
+import itertools
 
 
 class Command(BaseCommand):
-    help = 'Load Courses and Modules'
+	help = 'Load Courses and Modules'
 
-    def handle(self, *args, **kwargs):
-        raise NotImplementedError
+	def handle(self, *args, **kwargs):
+		if not GDP.objects.count():
+			datafile = settings.BASE_DIR / 'data' / 'gdp.json'
+
+			with open(datafile, 'r') as f:
+				data = json.load(f)
+
+			data = itertools.dropwhile(lambda x: x['Country_name'] != 'Afghanistan', data)
+
+		gdps = []
+		for d in data:
+			gdps.append(GDP(
+				country=d['Country_Name'],
+				country_cede=d['Country_Code'],
+				year=d['year'],
+				gdp=d['values']
+			))
+
+		GDP.objects.bulk_create(gdps)
--- gdp/models.py
+++ gdp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class GDP(models.Model):
+	country_code = models.CharField(max_length=100)
+	country = models.CharField(max_length=100)
+	year = models.PositiveSmallIntegerField()
+	gdp = models.FloatField()
+
+	def __str__(self):
+		return f"{self.country}"
